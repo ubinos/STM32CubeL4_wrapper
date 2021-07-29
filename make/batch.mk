79f59b13eb80fc72fffcbae53e7d131be57e9cce
Sungho Park <siminsungho@gmail.com>
2021-07-29 16:15:04 +0900

    add ubinos app, cli_tester_stm32l276geval_extdtty

--- make/batch.mk
+++ make/batch.mk
@@ -3,24 +3,40 @@
 %:
 	@echo ""
 	@echo ""
-	
-	
+
+
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../config CONFIG_NAME=stm32cubel4_stm32l475eiot01_baremetal
 	@echo ""
 	@echo ""
-	
-	
+
+
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../config CONFIG_NAME=stm32cubel4_stm32l476geval_baremetal
+	@echo ""
+	@echo ""
+
+
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
 	@echo ""
 	make -f makefile.mk $@ CONFIG_DIR=../app CONFIG_NAME=mybsp_stm32l475eiot01
 	@echo ""
 	@echo ""
-	
-	
+
+
+	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+	@echo ""
+	@echo ""
+	make -f makefile.mk $@ CONFIG_DIR=../app CONFIG_NAME=cli_tester_stm32l276geval_extdtty
+	@echo ""
+	@echo ""
+
+
 	@echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 	@echo ""
 
