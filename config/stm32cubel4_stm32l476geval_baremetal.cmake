337d928b1700d3ea628341dbbb5d333fe49068bd
Sungho Park <siminsungho@gmail.com>
2021-07-29 14:41:37 +0900

    add ubinos lib, stm32cubel4_stm32l476geval_baremetal

--- config/stm32cubel4_stm32l476geval_baremetal.cmake
+++ config/stm32cubel4_stm32l476geval_baremetal.cmake
@@ -6,7 +6,7 @@
 
 # {ubinos_config_type: [buildable, cmake, app]}
 
-include(${PROJECT_UBINOS_DIR}/config/ubinos_stm32l475eiot01_baremetal.cmake)
+include(${PROJECT_UBINOS_DIR}/config/ubinos_stm32l476geval_baremetal.cmake)
 
 include(${CMAKE_CURRENT_LIST_DIR}/stm32cubel4.cmake)
 
@@ -14,5 +14,5 @@ include(${CMAKE_CURRENT_LIST_DIR}/stm32cubel4.cmake)
 
 get_filename_component(_tmp_source_dir "${STM32CUBEL4__BASE_DIR}" ABSOLUTE)
 
-include_directories(${_tmp_source_dir}/Projects/B-L475E-IOT01A/Examples/BSP/Inc)
+include_directories(${_tmp_source_dir}/Projects/STM32L476G-EVAL/Examples/BSP/Inc)
 
